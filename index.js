98c82219b9e474d23dae327fa1af9b1c5c4db7d4
yeonjeonge-e <62794884+yeonjeonge-e@users.noreply.github.com>
2024-12-04 17:37:14 +0900

    Test commit for email update

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-let name = prompt("이름을 입력해 주세요.", "");
-alert(name);
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "동의하십니까?",
+//   function () {
+//     alert("동의하셨습니다.");
+//   },
+//   function () {
+//     alert("취소 버튼을 누르셨습니다.");
+//   }
+// );
+
+// function ask(question, yes, no) {
+//   if (confirm(question)) yes();
+//   else no();
+// }
+
+// ask(
+//   "동의하십니까?",
+//   () => alert("동의하셨습니다."),
+//   () => alert("취소 버튼을 누르셨습니다.")
+// );
